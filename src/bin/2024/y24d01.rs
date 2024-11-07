1f69580032769db9e23e3d13e63e67d48ae71de3
neil <neil.banman@gmail.com>
2024-11-07 08:18:39 -0800

    use std chunks instead of itertools

--- src/bin/2024/y24d01.rs
+++ src/bin/2024/y24d01.rs
@@ -1,5 +1,4 @@
 use everybody_codes::inputs::get_inputs;
-use itertools::Itertools;
 
 fn main() {
     let (input1, input2, input3) = get_inputs(24, 1);
@@ -11,17 +10,13 @@ fn main() {
 fn solve(input: &str, group_size: usize) -> usize {
     input
         .as_bytes()
-        .into_iter()
         .chunks(group_size)
-        .into_iter()
         .map(|baddies| {
-            let baddies: Vec<u8> = baddies.cloned().collect();
             let number_of_baddies = baddies
                 .iter()
                 .filter(|&&it| it != b'x')
                 .count();
-            // println!("baddies: {:?}, #: {number_of_baddies}", baddies);
-            let potions = |baddie: &u8| -> usize {
+            let value = |baddie: &u8| -> usize {
                 match &baddie {
                     b'A' => 0,
                     b'B' => 1,
@@ -30,9 +25,10 @@ fn solve(input: &str, group_size: usize) -> usize {
                     _ => 0,
                 }
             };
-            let bonus_potions = number_of_baddies * 
-                (number_of_baddies.checked_sub(1).unwrap_or_default());
-            bonus_potions + (baddies.iter().map(|baddie| potions(baddie)).sum::<usize>())
+            let potions: usize = baddies.iter().map(|baddie| value(baddie)).sum();
+            let bonus_potions: usize = number_of_baddies * 
+                number_of_baddies.checked_sub(1).unwrap_or_default();
+            potions + bonus_potions
         }).sum()
 }
 
