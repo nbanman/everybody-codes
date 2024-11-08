f1f61b82c101061475660307b4bf4e89c8b0a4c1
nbanman <neil.banman@gmail.com>
2024-11-07 22:04:25 -0800

    streamlined

--- src/inputs.rs
+++ src/inputs.rs
@@ -4,16 +4,17 @@ use itertools::Itertools;
 
 pub fn get_inputs(year: u16, day: u8) -> (String, String, String) {
     let year = if year > 2000 { year - 2000 } else { year };
-    let path = format!("./inputs/everybody_codes_e20{}_q{}_p", year, day);
     (1..=3).into_iter()
-        .map(|part| {
-            let mut path = path.clone();
-            path.push_str(&part.to_string());
-            path.push_str(".txt");
-            let mut file = File::open(&path).unwrap();
-            let mut contents = String::new();
-            file.read_to_string(&mut contents).unwrap();
-            contents
-        }).collect_tuple()
+        .map(|part| get_input(year, day, part))
+        .collect_tuple()
         .unwrap()
+}
+
+pub fn get_input(year: u16, day: u8, part: u8) -> String {
+    let path = 
+        format!("./inputs/everybody_codes_e20{}_q{}_p{}.txt", year, day, part);
+    let mut file = File::open(&path).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    contents
 }
