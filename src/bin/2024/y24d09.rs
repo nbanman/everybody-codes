d7890154e2d47fcfb1d5d5e5ef80215b70c20ddd
neil <neil.banman@gmail.com>
2024-11-15 19:14:19 -0800

    working on p3

--- src/bin/2024/y24d09.rs
+++ src/bin/2024/y24d09.rs
@@ -1,68 +1,88 @@
-use std::{cmp::min, usize};
+use std::usize;
 
-use everybody_codes::{get_numbers::ContainsNumbers, inputs::get_input, stopwatch::{ReportDuration, Stopwatch}};
+use everybody_codes::{get_numbers::ContainsNumbers, inputs::get_inputs, stopwatch::{ReportDuration, Stopwatch}, try_get::TryGet};
 use itertools::Itertools;
 
 fn main() {
     let mut stopwatch = Stopwatch::new();
     stopwatch.start();
-    // let (input1, input2, input3) = get_inputs(24, 9);
-    let input1 = get_input(24, 9, 1);
-    // let input2 = get_input(24, 9, 2);
+    let (input1, input2, input3) = get_inputs(24, 9);
     println!("Inputs loaded ({})", stopwatch.lap().report());
-    let stamps = [10, 5, 3, 1];
-    println!("1. {} ({})", solve(&input1, &stamps), stopwatch.lap().report());
-    // println!("2. {} ({})", solve(&input2), stopwatch.lap().report());
-    // println!("3. {} ({})", solve(&input3), stopwatch.lap().report());
+    println!("1. {} ({})", part1(&input1), stopwatch.lap().report());
+    println!("2. {} ({})", part2(&input2), stopwatch.lap().report());
+    println!("3. {} ({})", part3(&input3), stopwatch.lap().report());
     println!("Total: {}", stopwatch.stop().report());
 }
 
-fn solve(input: &str, stamps: &[usize]) -> usize {
-    let stamps: Vec<usize> = stamps.iter().cloned().sorted_unstable().rev().collect();
-    let brightnesses: Vec<usize> = input.get_numbers().sorted().rev().collect();
-    let mut cache: Vec<Option<usize>> = vec![None; brightnesses[0] + 1];
-    for stamp in &stamps {
+fn part1(input: &str) -> usize {
+    let stamps: Vec<usize> = [1, 3, 5, 10].into_iter().rev().collect();
+    let brightnesses = get_brightnesses(input);
+    let &brightest = brightnesses.iter().max().unwrap();
+    let mut cache: Vec<Option<usize>> = vec![None; brightest + 1];
+    solve(&brightnesses, &stamps, &mut cache)
+}
+
+fn part2(input: &str) -> usize {
+    let stamps: Vec<usize> = [1, 3, 5, 10, 15, 16, 20, 24, 25, 30].into_iter().rev().collect();
+    let brightnesses = get_brightnesses(input);
+    let &brightest = brightnesses.iter().max().unwrap();
+    let mut cache: Vec<Option<usize>> = vec![None; brightest + 1];
+    solve(&brightnesses, &stamps, &mut cache)
+}
+
+fn part3(input: &str) -> usize {
+    let stamps: Vec<usize> = [1, 3, 5, 10, 15, 16, 20, 24, 25, 30, 37, 38, 49, 50, 74, 75, 100, 101]
+        .into_iter()
+        .rev()
+        .collect();
+    let brightnesses = get_brightnesses(input);
+    let &brightest = brightnesses.iter().max().unwrap();
+    let mut cache: Vec<Option<usize>> = vec![None; brightest + 1];
+    
+} 
+
+fn get_brightnesses(input: &str) -> Vec<usize> {
+    input.get_numbers().collect()
+}
+
+fn solve(brightnesses: &[usize], stamps: &[usize], cache: &mut Vec<Option<usize>>) -> usize {
+    for stamp in stamps {
         cache[*stamp] = Some(1);
     }
-    let answer = brightnesses.iter()
+    brightnesses.iter()
         .map(|brightness| {
             if let Some(beetles) = cache[*brightness] {
                 beetles
             } else {
-                println!("\tFinding {brightness}");
-                let beetles = trav(*brightness, usize::MAX, &mut cache, &stamps);
-                println!("\t{brightness} takes {beetles} beetles");
+                let beetles = get_beetles(
+                    *brightness, 
+                    usize::MAX, 
+                    cache, 
+                    &stamps
+                );
                 beetles
             }
         })
-        .sum();
-    println!("{answer}");
-    answer
+        .sum()
 }
 
-fn trav (
+fn get_beetles (
     remaining: usize,
     prev_best: usize,
     cache: &mut Vec<Option<usize>>,
     stamps: &[usize],
 ) -> usize {
-    // cache hit; return precomputed answer
     if let Some(count) = cache[remaining] {
         return count;
     }
 
     // otherwise, carry on
-    let mut best = usize::MAX;
+    let mut best = prev_best;
 
     // iterate through each stamp in descending order
-    for (idx, &stamp) in stamps.iter().enumerate() {
+    'outer: for (idx, &stamp) in stamps.iter().enumerate() {
         let beetles = remaining / stamp;
-
-        // if the best possible outcome requries more beetles than prev_best, abort
-        if beetles >= prev_best {
-            return prev_best;
-        }
-
+        // if the best possible outcome requires more than the current best, abort
         if beetles >= best {
             return best;
         }
@@ -78,30 +98,41 @@ fn trav (
         // otherwise try decreasing amounts of that stamp
         // only runs if the stamp is smaller than the remaining brightness
         for n in (1..=beetles).rev() {  
+            // there should be break here where we know not to continue because anything
+            // remaining will result in something bigger than current best.
+            // maybe get the next stamp and do the calculation there...
+            if let Some(&next_stamp) = stamps.try_get(idx + 1) {
+                let next_stamp_beetles = ((remaining - n * stamp) as f32
+                    / next_stamp as f32).ceil() as usize;
+                if n + next_stamp_beetles >= best {
+                    break 'outer;
+                }
+            }
             // this should be the best obtainable with n number of stamp
-            if best <= n { continue; }
-            let result = n + trav(
+            if best <= n { 
+                continue; 
+            }
+            let result = n + get_beetles(
                 remaining - n * stamp,
-                best - n, // the -n is an experiment
+                best - n, 
                 cache,
                 &stamps[idx..],
             );
             if result < best { 
                 best = result; 
-            }
+            } 
         }
     }
     cache[remaining] = Some(best);
     best
 }
 
-#[test]
-fn tests() {
-    let tests = [r"2, 4, 7, 16", "27"];
-    let stamps = [[1, 3, 5, 10], [1, 3, 9, 10]];
-    assert_eq!(3, solve(tests[1], &stamps[1]));
-    assert_eq!(10, solve(tests[0], &stamps[0]));
-    // assert_eq!(9, part2(tests[1]));
-    // assert_eq!(9, part3(tests[2]));
-}
+// #[test]
+// fn tests() {
+//     let tests = [r"2, 4, 7, 16", "27", "156488, 352486, 546212"];
+//     let stamps = [vec![1, 3, 5, 10], vec![1, 3, 9, 10], vec![1, 3, 5, 10, 15, 16, 20, 24, 25, 30, 37, 38, 49, 50, 74, 75, 100, 101]];
+//     assert_eq!(10, part1(tests[0], &stamps[0]));
+//     assert_eq!(3, solve(tests[1], &stamps[1]));
+//     assert_eq!(10449, part3(tests[2], &stamps[2]));
+// }
 
