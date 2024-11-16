05cbde62f00d8e9c2473f6f674dfea30c80f4c83
nbanman <neil.banman@gmail.com>
2024-11-16 10:56:53 -0800

    pt3 parsing rules completed, need to process '?' squares,

--- src/bin/2024/y24d10.rs
+++ src/bin/2024/y24d10.rs
@@ -1,17 +1,15 @@
 use std::collections::HashSet;
 
-use everybody_codes::{inputs::get_input, stopwatch::{ReportDuration, Stopwatch}};
+use everybody_codes::{inputs::get_inputs, stopwatch::{ReportDuration, Stopwatch}};
 
 fn main() {
     let mut stopwatch = Stopwatch::new();
     stopwatch.start();
-    // let (input1, input2, input3) = get_inputs(24, 10);
-    let input1 = get_input(24, 10, 1);
-    let input2 = get_input(24, 10, 2);
+    let (input1, input2, input3) = get_inputs(24, 10);
     println!("Inputs loaded ({})", stopwatch.lap().report());
-    // println!("1. {} ({})", part1(&input1), stopwatch.lap().report());
-    println!("2. {} ({})", part2(&input2), stopwatch.lap().report());
-    // println!("3. {} ({})", solve(&input3), stopwatch.lap().report());
+    println!("1. {} ({})", part1(&input1), stopwatch.lap().report());
+    // println!("2. {} ({})", part2(&input2), stopwatch.lap().report());
+    println!("3. {} ({})", part3(&input3), stopwatch.lap().report());
     println!("Total: {}", stopwatch.stop().report());
 }
 
@@ -24,7 +22,7 @@ fn get_runes(length: usize, hz: &Vec<HashSet<char>>, vt: &Vec<HashSet<char>>) ->
     let mut runes = String::new();
     for y in 0..length {
         for x in 0..length {
-            let &cross = hz[y].intersection(&vt[x]).next().unwrap();
+            let &cross = hz[y].intersection(&vt[x]).next().unwrap_or(&'?');
             runes.push(cross);
         }
     }
@@ -32,7 +30,7 @@ fn get_runes(length: usize, hz: &Vec<HashSet<char>>, vt: &Vec<HashSet<char>>) ->
 }
 
 fn part2(input: &str) -> usize {
-    let samples: Vec<String> = get_samples(input);
+    let samples: Vec<String> = get_samples_spaced(input);
     samples.iter() 
         .map(|sample| {
             let (hz, vt, length) = crosstabs(sample);
@@ -44,16 +42,27 @@ fn part2(input: &str) -> usize {
         .sum()
 }
 
-fn get_samples(input: &str) -> Vec<String> {
+fn part3(input: &str) -> usize {
+    let samples: Vec<String> = get_samples_compressed(input);
+    samples.iter() 
+        .map(|sample| {
+            let (hz, vt, length) = crosstabs(sample);
+            let runes = get_runes(length, &hz, &vt);
+            3
+        })
+        .sum()
+}
+
+fn get_samples_spaced(input: &str) -> Vec<String> {
     let width = input.find('\n').unwrap() + 1;
     let rows: Vec<&str> = input.split("\n\n").collect();
     let samples_per_row = width / 9;
     rows.into_iter()
-        .flat_map(|row| get_row_samples(row, samples_per_row))
+        .flat_map(|row| get_row_samples_spaced(row, samples_per_row))
         .collect()
 }
 
-fn get_row_samples(row: &str, samples_per_row: usize) -> Vec<String> {
+fn get_row_samples_spaced(row: &str, samples_per_row: usize) -> Vec<String> {
     let mut samples = vec![String::new(); samples_per_row];
     for line in row.lines() {
         for (place, c) in line.chars().enumerate() {
@@ -68,6 +77,37 @@ fn get_row_samples(row: &str, samples_per_row: usize) -> Vec<String> {
     samples
 }
 
+fn get_samples_compressed(input: &str) -> Vec<String> {
+    let width = input.find('\n').unwrap() + 1;
+    let height = (input.len() + 1) / width;
+    let samples_per_row = (width - 2) / 6;
+    let rows = 0..((height - 2) / 6);
+    rows
+        .map(|row| {
+            let start = row * (width * 6);
+            let end = start + (width * 8) - 1; 
+            &input[start..end]
+        })
+        .flat_map(|row| get_row_samples_compressed(row, samples_per_row))
+        .collect()
+}
+
+fn get_row_samples_compressed(row: &str, samples_per_row: usize) -> Vec<String> {
+    let mut samples = vec![String::new(); samples_per_row];
+    for line in row.lines() {
+        let line = line.as_bytes();
+        for sample_idx in 0..samples_per_row {
+            let start = sample_idx * 6;
+            let end = start + 8;
+            let sample = &mut samples[sample_idx];
+            for place in start..end {
+                sample.push(line[place] as char);
+            }
+            sample.push('\n');
+        }
+    }
+    samples
+}
 
 fn crosstabs(sample: &str) -> (Vec<HashSet<char>>, Vec<HashSet<char>>, usize) {
     println!("called\n{sample}\n");
@@ -99,7 +139,7 @@ FL....JW
 SG....MN
 **FTZV**
 **GMJH**"];
-    assert_eq!(10, part1(inputs[0]));
+    // assert_eq!(10, part1(inputs[0]));
     // assert_eq!(10, solve(tests[1]));
     // assert_eq!(10, solve(tests[2]));
 }
